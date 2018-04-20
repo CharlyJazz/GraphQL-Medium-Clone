799177d22e11333403b0307ab48e2f363e96ed0c
charlyjazz <carlosjazzc1@gmail.com>
2018-04-19 21:25:27 -0400

    feat(create-post): title input
    Signed-off-by: charlyjazz <carlosjazzc1@gmail.com>

--- client/src/containers/NewPostContainer/index.js
+++ client/src/containers/NewPostContainer/index.js
@@ -7,17 +7,28 @@ import Divider from 'material-ui/Divider'
 import Button from 'material-ui/Button'
 import { createEditorState } from 'medium-draft'
 import mediumDraftExporter from 'medium-draft/lib/exporter'
+import { FormControl, FormHelperText } from 'material-ui/Form'
+import Input, { InputLabel } from 'material-ui/Input'
+import Typography from 'material-ui/Typography/Typography'
 
 const styles = theme => ({
   gridButton: {
     marginTop: '1rem',
     textAlign: 'right'
+  },
+  FormControl: {
+    marginLeft: '2rem'
   }
 })
 
 class NewPostContainer extends Component {
   state = {
-    editorState: createEditorState()
+    editorState: createEditorState(),
+    title: 'Your new history',
+    errors: {
+      title: null,
+      editor: null
+    }
   }
 
   onChange = (editorState) => {
@@ -30,14 +41,27 @@ class NewPostContainer extends Component {
 
   render() {
     let { classes } = this.props
+    const errors = this.state.errors
 
     return (
       <PaperCenter>
+        <FormControl className={classes.FormControl}>
+          <InputLabel htmlFor="title-input">Title of the history</InputLabel>
+          <Input id="title-input" name="title" value={this.state.title} onChange={
+            event => this.setState({ title: event.target.value })
+          } type="text"/>
+          {errors.title && 
+            <FormHelperText id="title-input-text">{errors.title}</FormHelperText>
+          }
+        </FormControl>
+
         <MyEditor
           editorState={this.state.editorState}
           onChange={this.onChange}
         />
+
         <Divider />
+
         <Grid item xs={12} className={classes.gridButton}>
           <Button raised color="primary" onClick={this.onSubmitPost}>
             Create
