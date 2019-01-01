0f45ad1b2922abd06122b1804e8f769fe9a2222d
Carlos Azuaje <carlosjazzc1@gmail.com>
2019-01-01 13:00:48 -0400

    feat(clean): remove client because i dont have time for code
    * refactor(delete-bullshit): clear directory
    
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>
    
    * refactor(delete-bullshit): add style components
    
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>
    
    * feat(delete): components and containers
    
    Signed-off-by: Carlos Azuaje <carlosjazzc1@gmail.com>

--- client/src/application/index.js
+++ client/src/application/index.js
@@ -3,18 +3,6 @@ import getCurrentUser from '../shared/getCurrentUser'
 import { graphql, compose } from 'react-apollo'
 import { SET_CURRENT_USER } from '../apollo/clientMutations'
 import { GET_CURRENT_USER } from '../apollo/clientQueries'
-import Grid from 'material-ui/Grid/Grid'
-import Header from '../components/Navegation/Header/Header'
-import { Switch, Route } from 'react-router-dom';
-import Home from '../containers/HomeContainer'
-import Auth from '../containers/AuthContainer'
-import Post from '../containers/PostContainer'
-import Search from '../containers/SearchContainer'
-import Profile from '../containers/ProfileContainer'
-import Collections from '../containers/CollectionsContainer'
-import Topics from '../containers/TopicsContainer'
-import NewPost from '../containers/NewPostContainer'
-import Logout from '../containers/LogoutContainer'
 
 class Application extends Component {
   componentWillReceiveProps(nextProps) {
@@ -35,31 +23,7 @@ class Application extends Component {
   }
 
   render() {
-    const { currentUser, loading } = this.props.getCurrentUser
-    return (
-      <React.Fragment>
-        <Header
-          loading={loading}
-          currentUser={currentUser}
-        />
-        <main style={{ paddingLeft: 20, paddingRight: 20 }}>
-          <Grid container spacing={0}>
-            <Switch>
-              <Route path='/' component={Home} exact />
-              <Route path='/logout' component={Logout} />
-              <Route path='/auth' component={Auth} />
-              <Route path='/post/:id' component={Post} />
-              <Route path='/search' component={Search} />
-              <Route path='/profile/:name' component={Profile} />
-              <Route path='/topics/:name' component={Topics} />
-              <Route path='/topics' component={Topics} />
-              <Route path='/collections' component={Collections} />
-              <Route path='/write/post' component={NewPost} />
-            </Switch>
-          </Grid>
-        </main>
-      </React.Fragment>
-    )
+    return null
   }
 }
 
